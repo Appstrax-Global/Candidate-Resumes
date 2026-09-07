8b75352073250b60d3fe91607a63587654362116
chaudharisakshi8686 <chaudharisakshi8686@gmail.com>
2026-09-07 09:11:27 +0530

    Add webhook

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  n8nWebhook: process.env.N8N_WEBHOOK || 'http://localhost:5678/webhook/document',
+  n8nWebhook: process.env.N8N_WEBHOOK || 'https://n8n-lnip.srv1936400.hstgr.cloud/webhook/e601a6d1-9dbf-4b9d-b2bb-69c6dec0bd45',
   maxFileSizeMb: parseInt(process.env.MAX_FILE_SIZE_MB || '20', 10),
   uploadDir: 'uploads',
   logDir: 'logs',
